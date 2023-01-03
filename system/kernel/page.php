e4a3223a8bc929ef4f6395ea08672305a1ed5a76
namdd <namdd@smartosc.com>
2023-01-03 17:46:33 +0700

    ver

--- system/kernel/page.php
+++ system/kernel/page.php
@@ -167,6 +167,7 @@ class Page
         $this->header .= $this->setAdsGoogleAdsense();
         $this->header .= '
         <link type="text/css" rel="stylesheet" href="' . $this->host . '/webskins/skins/news/css/jquery.mmenu.all.css" />
+        <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
         integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
